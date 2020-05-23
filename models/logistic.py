0c7590d477d4a3af242c348cd6aa21f93efd1018
Luke <olooook@outlook.com>
2020-05-23 18:21:59 +0800

    修改为staticmethod

--- models/logistic.py
+++ models/logistic.py
@@ -17,32 +17,33 @@ class LogisticRegression:
         self.lr = lr
 
     def fit(self, X, Y):
-        X = pad(X)
-        pred = sigmoid(X @ self.weights)
-        grad = X.T @ (pred - Y) / len(pred)
+        x_pad = self.pad(X)
+        pred = self.sigmoid(x_pad @ self.weights)
+        grad = x_pad.T @ (pred - Y) / len(pred)
         self.weights -= self.lr * grad
 
     def predict(self, X):
-        pred = sigmoid(pad(X) @ self.weights)
-        return binarize(pred)
+        x_pad = self.pad(X)
+        pred = self.sigmoid(x_pad @ self.weights)
+        return self.binarize(pred)
 
+    @staticmethod
+    def pad(x):
+        return np.concatenate([x, np.ones([len(x), 1])], axis=1)
 
-def pad(x):
-    return np.concatenate([x, np.ones([len(x), 1])], axis=1)
+    @staticmethod
+    def sigmoid(x):
+        """
+        :param x:
+        :return: \frac{1}{1 + e^{-x}}
+        """
+        return 1 / (1 + np.exp(-x))
 
-
-def sigmoid(x):
-    """
-    :param x:
-    :return: \frac{1}{1 + e^{-x}}
-    """
-    return 1 / (1 + np.exp(-x))
-
-
-def binarize(x, threshold=.5):
-    b = np.zeros([len(x)])
-    b[x > threshold] = 1
-    return b
+    @staticmethod
+    def binarize(x, threshold=.5):
+        b = np.zeros([len(x)])
+        b[x > threshold] = 1
+        return b
 
 
 """
