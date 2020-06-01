cd930b02ded209792d1c26916759c476cba5c15e
Luke <olooook@outlook.com>
2020-06-01 11:56:26 +0800

    添加注释

--- samples/bayes.py
+++ samples/bayes.py
@@ -28,8 +28,15 @@ def test_naive_bayes():
 
     bayes = NaiveBayesClassifier([3, 3], 2)
     bayes.fit(X, Y)
-    print(bayes.prior_prob)  # 计算先验概率
-    print(bayes.cond_prob)  # 计算条件概率
+
+    print(bayes.prior_prob)  # 先验概率
+    # [10/17, 7/17]
+
+    print(bayes.cond_prob)  # 条件概率
+    # [4/9, 3/9, 2/9]
+    # [4/9, 3/9, 2/9]
+    # [3/12, 4/12, 5/12]
+    # [2/12, 5/12, 5/12]
 
 
 if __name__ == "__main__":
