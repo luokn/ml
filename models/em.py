00cc0fafc8db6b213f8a01c85c5043d20e130875
Luke <olooook@outlook.com>
2020-06-05 13:09:42 +0800

    .

--- models/em.py
+++ models/em.py
@@ -13,24 +13,21 @@ class SimpleEM:
 
     def __init__(self, prob: list, max_iter=100):
         self.prob, self.max_iter = np.array(prob), max_iter
-        self.X, self._mu = None, None
 
     def fit(self, X: np.ndarray):
-        self.X = X
         for _ in range(self.max_iter):
-            self._expect()
-            self._maximize()
-        self._mu = None
+            M = self._expect(X)
+            self._maximize(X, M)
 
-    def _expect(self):  # E步
+    def _expect(self, X: np.ndarray):  # E步
         p1, p2, p3 = self.prob
-        a = p1 * (p2 ** self.X) * ((1 - p2) ** (1 - self.X))
-        b = (1 - p1) * (p3 ** self.X) * ((1 - p3) ** (1 - self.X))
-        self._mu = a / (a + b)
+        a = p1 * (p2 ** X) * ((1 - p2) ** (1 - X))
+        b = (1 - p1) * (p3 ** X) * ((1 - p3) ** (1 - X))
+        return a / (a + b)
 
-    def _maximize(self):  # M步
-        self.prob[0] = np.sum(self._mu) / len(self.X)
-        self.prob[1] = np.sum(self._mu * self.X) / np.sum(self._mu)
-        self.prob[2] = np.sum((1 - self._mu) * self.X) / np.sum(1 - self._mu)
+    def _maximize(self, X: np.ndarray, M: np.ndarray):  # M步
+        self.prob[0] = np.sum(M) / len(X)
+        self.prob[1] = np.sum(M * X) / np.sum(M)
+        self.prob[2] = np.sum((1 - M) * X) / np.sum(1 - M)
 
 # EM算法与高斯混合模型可参见./gmm.py
