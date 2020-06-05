875b3cc299c35629d0fac3583e0aaa689f91c23c
Luke <olooook@outlook.com>
2020-06-05 17:01:24 +0800

    .

--- models/knn.py
+++ models/knn.py
@@ -15,16 +15,16 @@ class KNN:
         """
         :param k: 分类近邻数
         """
-        self.k, self.x_train, self.y_train = k, None, None
+        self.k, self.X, self.Y = k, None, None
 
     def fit(self, X: np.ndarray, Y: np.ndarray):
-        self.x_train, self.y_train = X, Y  # 训练集X与Y，类别已知
+        self.X, self.Y = X, Y  # 训练集X与Y，类别已知
 
     def predict(self, X: np.ndarray):
         Y = np.zeros([len(X)], dtype=int)  # X对应的类别输出变量
         for i, x in enumerate(X):
-            dist = np.linalg.norm(self.x_train - x, axis=1)  # 计算x与所有已知类别点的距离
+            dist = np.linalg.norm(self.X - x, axis=1)  # 计算x与所有已知类别点的距离
             topk = np.argsort(dist)[:self.k]  # 取距离最近的k个点对应的索引
-            counter = np.bincount(self.y_train[topk])  # 统计k近邻点的类别数量
+            counter = np.bincount(self.Y[topk])  # 统计k近邻点的类别数量
             Y[i] = np.argmax(counter)  # k近邻次数最多的类别将作为x的类别
         return Y
