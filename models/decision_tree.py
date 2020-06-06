4af997e0e041c1eba3fffa6140b5e244ccfb1e8e
Luke <olooook@outlook.com>
2020-06-06 16:36:02 +0800

    简化

--- models/decision_tree.py
+++ models/decision_tree.py
@@ -14,12 +14,12 @@ class DecisionTree:
     def __init__(self, rate: float = 0.95):
         self.rate = rate
         self.tree = None
-        self.x_train, self.y_train = None, None
+        self.X, self.Y = None, None
 
     def fit(self, X: np.ndarray, Y: np.ndarray):
         indices = np.arange(X.shape[0])  # 所有行索引
         features = np.arange(X.shape[1])  # 所有列索引
-        self.x_train, self.y_train = X, Y
+        self.X, self.Y = X, Y
         self.tree = self._create_tree(indices, features)
 
     def predict(self, X: np.ndarray):
@@ -41,13 +41,13 @@ class DecisionTree:
         best_feature = self._select_feature(indices, features)  # 选择香农熵最小的特征
         trees = {}
         rest_features = features[features != best_feature]  # 除去选择的特征
-        for val in np.unique(self.x_train[indices, best_feature]):  # 为该特征的每一个取值都建立子树
+        for val in np.unique(self.X[indices, best_feature]):  # 为该特征的每一个取值都建立子树
             sub_indices = self._query_indices(indices, best_feature, val)
             trees[int(val)] = self._create_tree(sub_indices, rest_features)  # 递归构建子决策树
         return {'feature': best_feature, 'trees': trees}
 
     def _query_indices(self, indices, feature, value):
-        return indices[self.x_train[indices, feature] == value]
+        return indices[self.X[indices, feature] == value]
 
     def _calc_info_gain(self, indices, feature):  # 计算信息增益
         ent = self._calc_entropy(indices)  # 经验熵
@@ -55,19 +55,19 @@ class DecisionTree:
         return ent - cond_ent  # 信息增益
 
     def _calc_entropy(self, indices):  # 计算经验熵
-        prob = np.bincount(self.y_train[indices]) / len(indices)  # 采用二进制计数法，x必须为正整数向量
+        prob = np.bincount(self.Y[indices]) / len(indices)  # 采用二进制计数法，x必须为正整数向量
         prob = prob[prob != 0]  # 除去0概率
         return np.sum(prob * -np.log(prob))  # 经验熵
 
     def _calc_cond_entropy(self, indices, feature):  # 计算条件熵
         cond_ent = 0  # 经验条件熵
-        for val in np.unique(self.x_train[indices, feature]):
+        for val in np.unique(self.X[indices, feature]):
             sub_indices = self._query_indices(indices, feature, val)
             cond_ent += len(sub_indices) / len(indices) * self._calc_entropy(sub_indices)
         return cond_ent  # 条件熵
 
     def _select_class(self, indices):
-        prob = np.bincount(self.y_train[indices]) / len(indices)  # 计算类别频率
+        prob = np.bincount(self.Y[indices]) / len(indices)  # 计算类别频率
         c = np.argmax(prob)
         return c, prob[c]  # 返回出现次数最多的类别，以及其频率
 
