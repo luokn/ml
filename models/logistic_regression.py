8d0f1dc1784ee5bebdaf30bcbc797ea920f6dd69
Luke <olooook@outlook.com>
2020-06-06 16:37:38 +0800

    简化

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -16,20 +16,20 @@ class LogisticRegression:
         self.lr = lr  # 学习率
 
     def fit(self, X: np.ndarray, Y: np.ndarray):
-        x_pad = pad(X)  # 为X填充1作为偏置
-        pred = sigmoid(x_pad @ self.weights)  # 计算预测值
+        x_pad = self.pad(X)  # 为X填充1作为偏置
+        pred = self.sigmoid(x_pad @ self.weights)  # 计算预测值
         grad = x_pad.T @ (pred - Y) / len(pred)  # 计算梯度
         self.weights -= self.lr * grad  # 沿负梯度更新参数
 
     def predict(self, X: np.ndarray):
-        x_pad = pad(X)  # 为X填充1作为偏置
-        pred = sigmoid(x_pad @ self.weights)  # 计算预测值
+        x_pad = self.pad(X)  # 为X填充1作为偏置
+        pred = self.sigmoid(x_pad @ self.weights)  # 计算预测值
         return np.where(pred > 0.5, 1, 0)  # 将(0, 1)之间分布的概率转化为{0, 1}标签
 
+    @staticmethod
+    def pad(x):
+        return np.concatenate([x, np.ones([len(x), 1])], axis=1)
 
-def pad(x):
-    return np.concatenate([x, np.ones([len(x), 1])], axis=1)
-
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
