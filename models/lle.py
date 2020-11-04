8e6dc996bceee592932ff7a1c2f1c028ef7d264c
Luokun <olooook@outlook.com>
2020-11-04 13:23:37 +0800

    .

--- models/lle.py
+++ models/lle.py
@@ -20,7 +20,7 @@ class LLE:
         """
         self.k, self.d = k, d
 
-    def transform(self, X: np.array) -> np.array:
+    def transform(self, X: np.array):
         """
         Args:
             X (np.array): shape为 M × c
@@ -31,8 +31,8 @@ class LLE:
         for i, x in enumerate(X):
             topk = np.linalg.norm(x - X, axis=1).argsort()[1:self.k + 1]  # 找出距离最近的k个点的下标，排除自身
             w = np.linalg.inv(X[topk] @ X[topk].T).sum(axis=1)  # 计算w
-            W[i, topk] = w / w.sum()  # 归一化, 扩展到所有点上
+            W[i, topk] = w / w.sum()  # 归一化并扩展到所有点上
         W = np.eye(len(X)) - W
-        L, U = np.linalg.eig(W @ W.T)  # 对M进行特征值分解
+        L, U = np.linalg.eig(W @ W.T)  # 对M = (I - W)(I - W)^T进行特征值分解
         topd = L.argsort()[1:self.d + 1]  # 选取前d个非0特征值
         return U[topd].T  # 降维后的数据为选取的特征值对应的特征向量
--- samples/lle.py
+++ samples/lle.py
@@ -1,4 +1,29 @@
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from models.lle import LLE
 
+# %%
 
+if __name__ == "__main__":
+    lle = LLE(3, 1)
+    X = np.random.randn(10, 3)
+    Y = lle.transform(X)
+    print(Y)
+
+# %%
+
+# %%
+X = np.random.uniform(size=[1000, 2])
+C = np.zeros([1000, 3])
+C[:, 0] = X[:, 0]
+C[:, 1] = 1 - X[:, 0]
+C[:, 2] = 0.5
+X[:, 0] *= 3 * np.pi
+# %%
+plt.figure(figsize=(30, 10))
+plt.scatter(X[:, 0], X[:, 1], c=C)
+# %%
+
+# %%
