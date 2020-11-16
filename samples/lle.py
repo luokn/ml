bec67efcc306bf91f03d116681da7e4b027713db
Luokun <olooook@outlook.com>
2020-11-16 10:11:27 +0800

    .

--- samples/lle.py
+++ samples/lle.py
@@ -1,32 +1,39 @@
-# %%
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
-# %%
+
+def make_swiss_roll(n_samples=100, noise=0.0, shuffle=True):
+    t = 1.5 * np.pi * (1 + 2 * np.linspace(0, 1, n_samples))
+    X = np.stack([
+        t * np.cos(t),
+        t * np.sin(t),
+        100 * np.random.rand(n_samples),
+    ], axis=-1) + noise * np.random.randn(n_samples, 3)  # N × (x, y, z)
+    C = np.stack([
+        np.linspace(0.2, 1, n_samples),
+        np.linspace(1, 0.2, n_samples),
+        np.repeat(.5, n_samples)
+    ], axis=-1)  # N × (r, g, b)
+    if shuffle:
+        index = np.arange(n_samples).astype(np.int)
+        np.random.shuffle(index)
+        X, C = X[index], C[index]
+    return X, C
+
 
 if __name__ == "__main__":
-    import sys
     from os.path import dirname
     sys.path.append(dirname(dirname(__file__)))
     from models.lle import LLE
+    n_samples = 10000
     lle = LLE(3, 1)
-    X = np.random.randn(10, 3)
-    Y = lle.transform(X)
-    print(Y)
-
-# %%
-
-# %%
-X = np.random.uniform(size=[1000, 2])
-C = np.zeros([1000, 3])
-C[:, 0] = X[:, 0]
-C[:, 1] = 1 - X[:, 0]
-C[:, 2] = 0.5
-X[:, 0] *= 3 * np.pi
-# %%
-plt.figure(figsize=(30, 10))
-plt.scatter(X[:, 0], X[:, 1], c=C)
-# %%
-
-# %%
+    X, C = make_swiss_roll(n_samples, 0.1)
+    ax = Axes3D(plt.figure(figsize=(8, 8)))
+    ax.scatter(*X.T, c=C, marker='.')
+    plt.show()
+    # Y = lle.transform(X)
+    # plt.figure(figsize=(8, 8))
+    # plt.scatter(Y[:, 0], np.repeat(0, n_samples), c=C)
+    # plt.show()
