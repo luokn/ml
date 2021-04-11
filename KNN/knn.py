71060e4e905c960a4c34ae81a40b13104b602bdc
Luokun <olooook@outlook.com>
2021-04-11 20:53:20 +0800

    KNN/knn: 改进KNN

--- KNN/knn.py
+++ KNN/knn.py
@@ -17,27 +17,28 @@ class KNN:
         :param K: 分类近邻数
         """
         self.k = k
-        self._X, self._Y = None, None
+        self.X, self.Y = None, None
 
     def fit(self, X: np.ndarray, Y: np.ndarray):
-        self._X, self._Y = X, Y  # 训练集X与Y，类别已知
+        self.X, self.Y = X, Y  # 训练集X与Y，类别已知
 
-    def predict(self, X: np.ndarray):
+    def __call__(self, X: np.ndarray):
         Y = np.zeros([len(X)], dtype=int)  # X对应的类别输出变量
         for i, x in enumerate(X):
-            dist = np.linalg.norm(self._X - x, axis=1)  # 计算x与所有已知类别点的距离
+            dist = np.linalg.norm(self.X - x, axis=1)  # 计算x与所有已知类别点的距离
             topk = np.argsort(dist)[:self.k]  # 取距离最近的k个点对应的索引
-            counter = np.bincount(self._Y[topk])  # 统计k近邻点的类别数量
+            counter = np.bincount(self.Y[topk])  # 统计k近邻点的类别数量
             Y[i] = np.argmax(counter)  # k近邻次数最多的类别将作为x的类别
         return Y
 
 
 def load_data():
-    x, y = np.random.randn(3, 200, 2), np.zeros([3, 200])
-    x[0] += np.array([2, 2])  # 右偏移2，上偏移2
-    x[1] += np.array([2, -2])  # 右偏移2，下偏移2
-    y[1] = 1
-    y[2] = 2
+    x = np.stack([
+        np.random.randn(200, 2) + np.array([2, 2]),
+        np.random.randn(200, 2),
+        np.random.randn(200, 2) + np.array([2, -2]),
+    ])
+    y = np.stack([np.full([200], 0), np.full([200], 1), np.full([200], 2)])
     return x, y
 
 
@@ -51,18 +52,25 @@ def plot_scatter(xys, title):
 
 if __name__ == '__main__':
     x, y = load_data()
-    plot_scatter(x, 'Real')
 
-    # train
-    x = x.reshape(-1, 2)
-    y = y.flatten()
+    plt.figure(figsize=[12, 6])
+    plt.subplot(1, 2, 1)
+    plt.title('Real')
+    plt.scatter(x[0, :, 0], x[0, :, 1], color='r', marker='.')
+    plt.scatter(x[1, :, 0], x[1, :, 1], color='g', marker='.')
+    plt.scatter(x[2, :, 0], x[2, :, 1], color='b', marker='.')
+
+    x, y = x.reshape(-1, 2), y.flatten()
     knn = KNN(3)
     knn.fit(x, y)
-
-    # predict
-    pred = knn.predict(x)
-    plot_scatter([x[pred == i] for i in [0, 1, 2]], 'Pred')
-
-    # accuracy
+    pred = knn(x)
     acc = np.sum(pred == y) / len(pred)
     print(f'Acc = {100 * acc:.2f}%')
+
+    z = [x[pred == c] for c in [0, 1, 2]]
+    plt.subplot(1, 2, 2)
+    plt.title('Pred')
+    plt.scatter(z[0][:, 0], z[0][:, 1], color='r', marker='.')
+    plt.scatter(z[1][:, 0], z[1][:, 1], color='g', marker='.')
+    plt.scatter(z[2][:, 0], z[2][:, 1], color='b', marker='.')
+    plt.show()
