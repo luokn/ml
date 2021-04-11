3ce15977330f4939d6cd2cd8b81eb9b0be4cf4b6
Luokun <olooook@outlook.com>
2021-04-11 20:47:51 +0800

    Kmeans/kmeans: 改善代码

--- Kmeans/kmeans.py
+++ Kmeans/kmeans.py
@@ -3,11 +3,10 @@
 # @Author: Luokun
 # @Email : olooook@outlook.com
 
-import matplotlib.pyplot as plt
 import random
 
+import matplotlib.pyplot as plt
 import numpy as np
-from numpy.lib.npyio import load
 
 
 class KMeans:
@@ -15,19 +14,19 @@ class KMeans:
     K-means clustering(K均值聚类)
     """
 
-    def __init__(self, K: int, eps=1e-3, max_iter=100):
+    def __init__(self, K: int, eps=1e-3, iterations=100):
         """
         :param K: 聚类类别数
         :param eps: 中心点最小更新量
         :param max_iter: 迭代最大次数
         """
-        self.K, self.eps, self.max_iter = K, eps, max_iter
+        self.K, self.eps, self.iterations = K, eps, iterations
         self.centers = None  # 中心点
 
     def fit(self, X: np.ndarray):
         self.centers = X[random.sample(range(len(X)), self.K)]  # 随机选择k个点作为中心点
-        for _ in range(self.max_iter):  # 达到最大迭代次数iterations退出迭代
-            Y = self.predict(X)  # 更新节点类别
+        for _ in range(self.iterations):  # 达到最大迭代次数iterations退出迭代
+            Y = self.__call__(X)  # 更新节点类别
             means = np.empty_like(self.centers)  # 各类别点的均值
             for k in range(self.K):
                 if np.any(Y == k):  # 存在元素属于类别i
@@ -39,7 +38,7 @@ class KMeans:
                 break  # 退出迭代
             self.centers = means  # 将更新后的均值作为各类别中心点
 
-    def predict(self, X: np.ndarray):
+    def __call__(self, X: np.ndarray):
         Y = np.empty([len(X)], dtype=int)  # 类别
         for i, x in enumerate(X):
             Y[i] = np.linalg.norm(self.centers - x, axis=1).argmin()  # 每一点类别为最近的中心点类别
@@ -47,35 +46,34 @@ class KMeans:
 
 
 def load_data():
-    x = np.random.randn(3, 200, 2)
-    x[1] += np.array([2, 2])  # 右偏移2，上偏移2
-    x[2] += np.array([2, -2])  # 右偏移2，下偏移2
+    x = np.stack([
+        np.random.randn(200, 2) + np.array([2, 2]),
+        np.random.randn(200, 2),
+        np.random.randn(200, 2) + np.array([2, -2]),
+    ])
     return x
-
-
-def plot_scatter(xys, title):
-    plt.figure(figsize=[8, 8])
-    for xy, color in zip(xys, ['r', 'g', 'b']):
-        plt.scatter(xy[:, 0], xy[:, 1], color=color, marker='.')
-    plt.title(title)
-    plt.show()
-
-
-def plot_scatter_with_centers(xys, centers, title):
-    plt.figure(figsize=[8, 8])
-    for xy, center, color in zip(xys, centers, ['r', 'g', 'b']):
-        plt.scatter(xy[:, 0], xy[:, 1], color=color, marker='.')
-        plt.scatter(center[0], center[1], color=color, s=100, marker='*')
-    plt.title(title)
-    plt.show()
 
 
 if __name__ == '__main__':
     x = load_data()
-    plot_scatter(x, 'Real')
+
+    plt.figure(figsize=[12, 6])
+    plt.subplot(1, 2, 1)
+    plt.title('Real')
+    plt.scatter(x[0, :, 0], x[0, :, 1], color='r', marker='.')
+    plt.scatter(x[1, :, 0], x[1, :, 1], color='g', marker='.')
+    plt.scatter(x[2, :, 0], x[2, :, 1], color='b', marker='.')
 
     x = x.reshape(-1, 2)
     kmeans = KMeans(3)
-    pred = kmeans.predict(x)
+    kmeans.fit(x)
+    pred = kmeans(x)
 
-    plot_scatter_with_centers([x[pred == i] for i in [0, 1, 2]], kmeans.centers, 'Pred')
+    z = [x[pred == c] for c in [0, 1, 2]]
+    plt.subplot(1, 2, 2)
+    plt.title('Pred')
+    plt.scatter(z[0][:, 0], z[0][:, 1], color='r', marker='.')
+    plt.scatter(z[1][:, 0], z[1][:, 1], color='g', marker='.')
+    plt.scatter(z[2][:, 0], z[2][:, 1], color='b', marker='.')
+    plt.scatter(kmeans.centers[:, 0], kmeans.centers[:, 1], color=['r', 'g', 'b'],  marker='*', s=100)
+    plt.show()
