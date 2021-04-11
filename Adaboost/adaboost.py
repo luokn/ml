aceb375385e88a7f8530af9ce68f90c3febe46b7
Luokun <olooook@outlook.com>
2021-04-11 20:30:08 +0800

    改进Adaboost和决策树算法

--- Adaboost/adaboost.py
+++ Adaboost/adaboost.py
@@ -20,91 +20,81 @@ class AdaBoost:
         self.n_estimators, self.lr, self.eps = n_estimators, lr, eps
         self.estimators = []
         self.alpha = np.empty([n_estimators])  # 弱分类器权重
-        self._X, self._Y, self._weights = None, None, None
 
     def fit(self, X: np.ndarray, Y: np.ndarray):
-        self._X, self._Y, self._weights = X, Y, np.full([len(X)], 1 / len(X))
-        estimators = [self._WeakEstimator(f, self.lr) for f in range(X.shape[1])]  # 所有特征的弱分类器
+        weights = np.full([len(X)], 1 / len(X))  # 样本权重
+        estimators = [WeakEstimator(f, self.lr) for f in range(X.shape[1])]  # 所有特征的弱分类器
         predictions = np.zeros([len(estimators), len(Y)])  # 弱分类器输出
         for i, estimator in enumerate(estimators):
             estimator.fit(X, Y)  # 逐特征训练弱分类器
             predictions[i] = estimator(X)  # 记录所有弱分类器输出
         for k in range(self.n_estimators):
             errors = np.array([
-                np.sum(np.where(pred == Y, 0, self._weights)) for pred in predictions
+                np.sum(np.where(pred == Y, 0, weights)) for pred in predictions
             ])  # 计算每一个弱分类器的带权重误差
             idx = np.argmin(errors).item()  # 选择最小误差
             if errors[idx] < self.eps:  # 误差达到阈值，停止
                 break
             self.estimators.append(estimators[idx])  # 添加弱分类器
-            self._update_alpha(k, errors[idx])  # 更新弱分类器权重
-            self._update_weights(k, predictions[idx])  # 更新样本权重
-        self._X, self._Y, self._weights = None, None, None
+            self.alpha[k] = .5 * np.log((1 - errors[idx]) / errors[idx])  # 更新弱分类器权重
+            exp_res = np.exp(-self.alpha[k] * Y * predictions[idx])
+            weights *= exp_res / (weights @ exp_res)  # 更新样本权重
 
-    def predict(self, X: np.ndarray):
+    def __call__(self, X: np.ndarray):
         pred = np.array([
             alpha * estimator(X) for alpha, estimator in zip(self.alpha, self.estimators)
         ])
         pred = np.sum(pred, axis=0)
         return np.where(pred > 0, 1, -1)
 
-    def _update_alpha(self, k: int, e: float):  # 更新弱分类器权重
-        self.alpha[k] = .5 * np.log((1 - e) / e)
 
-    def _update_weights(self, k: int, pred: np.ndarray):  # 更新样本权重
-        exp_res = np.exp(-self.alpha[k] * self._Y * pred)
-        self._weights = self._weights * exp_res / (self._weights @ exp_res)
+class WeakEstimator:  # 弱分类器, 一阶决策树
+    def __init__(self, feature: int, lr: float):
+        self.feature, self.lr = feature, lr  # 划分特征、学习率
+        self.div, self.sign = None, None  # 划分值、符号
 
-    class _WeakEstimator:  # 弱分类器, 一阶决策树
-        def __init__(self, feature: int, lr: float):
-            self.feature, self.lr = feature, lr  # 划分特征、学习率
-            self.div, self.sign = None, None  # 划分值、符号
+    def fit(self, X: np.ndarray, Y: np.ndarray):
+        x, max_corr = X[:, self.feature], 0
+        for value in np.arange(x.min(), x.max() + self.lr, self.lr):
+            pos_corr = np.sum(np.where(x > value, 1, -1) == Y)
+            neg_corr = len(x) - pos_corr
+            if pos_corr > max_corr:
+                self.div, self.sign, max_corr = value, 1, pos_corr
+            elif neg_corr > max_corr:
+                self.div, self.sign, max_corr = value, -1, neg_corr
 
-        def fit(self, X: np.ndarray, Y: np.ndarray):
-            Xf, max_corr = X[:, self.feature], 0
-            for value in np.arange(Xf.min(), Xf.max() + self.lr, self.lr):
-                pos_corr = np.sum(np.where(Xf > value, 1, -1) == Y)
-                neg_corr = len(Xf) - pos_corr
-                if pos_corr > max_corr:
-                    self.div, self.sign, max_corr = value, 1, pos_corr
-                elif neg_corr > max_corr:
-                    self.div, self.sign, max_corr = value, -1, neg_corr
-
-        def __call__(self, X: np.ndarray):
-            return np.where(X[:, self.feature] > self.div, self.sign, -self.sign)
+    def __call__(self, X: np.ndarray):
+        return np.where(X[:, self.feature] > self.div, self.sign, -self.sign)
 
 
 def load_data():
-    x, y = np.random.randn(2, 200, 2), np.zeros([2, 200])
-    x[0] += np.array([-1, 0])
-    x[1] += np.array([1, -1])
-    y[0] = -1
-    y[1] = 1
+    x = np.stack([
+        np.random.randn(200, 2) + np.array([-1, 0]),
+        np.random.randn(200, 2) + np.array([1, -1])
+    ])
+    y = np.stack([np.full([200], -1), np.full([200], 1)])
     return x, y
-
-
-def plot_scatter(xys, title):
-    plt.figure(figsize=(10, 10))
-    for xy, color in zip(xys, ['r', 'g', 'b']):
-        plt.scatter(xy[:, 0], xy[:, 1], color=color)
-    plt.title(title)
-    plt.show()
 
 
 if __name__ == '__main__':
     x, y = load_data()
-    plot_scatter(x, 'Real')
 
-    x = x.reshape(-1, 2)
-    y = y.flatten()
+    plt.figure(figsize=[10, 5])
+    plt.subplot(1, 2, 1)
+    plt.title('Real')
+    plt.scatter(x[0, :, 0], x[0, :, 1], color='r', marker='.')
+    plt.scatter(x[1, :, 0], x[1, :, 1], color='g', marker='.')
 
-    # train
-    ab = AdaBoost(5)
-    ab.fit(x, y)
-
-    pred = ab.predict(x)
-    plot_scatter([x[pred == i] for i in [-1, 1]], 'Pred')
-
-    # print accuracy
+    x, y = x.reshape(-1, 2), y.flatten()
+    ada_boost = AdaBoost(5)
+    ada_boost.fit(x, y)
+    pred = ada_boost(x)
     acc = np.sum(pred == y) / len(pred)
     print(f'Acc = {100 * acc:.2f}%')
+
+    z = x[pred == -1], x[pred == 1]
+    plt.subplot(1, 2, 2)
+    plt.title('Pred')
+    plt.scatter(z[0][:, 0], z[0][:, 1], color='r', marker='.')
+    plt.scatter(z[1][:, 0], z[1][:, 1], color='g', marker='.')
+    plt.show()
--- DecisionTree/decision_tree.py
+++ DecisionTree/decision_tree.py
@@ -4,7 +4,6 @@
 # @Email : olooook@outlook.com
 
 import numpy as np
-from numpy.lib.npyio import load
 
 
 class DecisionTree:
@@ -13,17 +12,14 @@ class DecisionTree:
     """
 
     def __init__(self, rate: float = 0.95):
-        self.rate = rate
-        self._X, self._Y, self.tree = None, None, None
+        self.rate, self.tree = rate, None
 
     def fit(self, X: np.ndarray, Y: np.ndarray):
-        self._X, self._Y = X, Y
         indices = np.arange(X.shape[0])  # 所有行索引
         features = np.arange(X.shape[1])  # 所有列索引
-        self.tree = self._create_tree(indices, features)
-        self._X, self._Y = None, None
+        self.tree = self._create_tree(X, Y, indices, features)
 
-    def predict(self, X: np.ndarray):
+    def __call__(self, X: np.ndarray):
         Y = np.zeros([len(X)], dtype=int)  # 输出变量
         for i, x in enumerate(X):
             Y[i] = self._predict(self.tree, x)
@@ -31,86 +27,79 @@ class DecisionTree:
 
     def _predict(self, node, x):
         if isinstance(node, dict):  # 如果节点是树(字典)类型
-            key = x[node['feature']]  # 获取划分特征的值
-            return self._predict(node['trees'][key], x)  # 根据值进行下一次递归
+            feature, trees = node['feature'], node['trees']
+            return self._predict(trees[x[feature]], x)  # 根据值进行下一次递归
         return node  # 如果节点是叶子类型则直接返回该值
 
-    def _create_tree(self, indices, features):
-        best_class, rate = self._select_class(indices)  # 获得数量最多的类别及其频率
+    def _create_tree(self, X, Y, indices, features):
+        c, rate = self._select_class(X, Y, indices)  # 获得数量最多的类别及其频率
         if len(features) == 0 or rate > self.rate:  # 无特征可分或者满足一定的单一性
-            return best_class  # 返回最单一的类别
-        best_feature = self._select_feature(indices, features)  # 选择香农熵最小的特征
-        sub_trees = {}
-        rest_features = features[features != best_feature]  # 除去选择的特征
-        for key in np.unique(self._X[indices, best_feature]):  # 为该特征的每一个取值都建立子树
-            sub_indices = self._query_indices(indices, best_feature, key)
-            sub_trees[key.item()] = self._create_tree(sub_indices, rest_features)  # 递归构建子决策树
-        return {'feature': best_feature, 'trees': sub_trees}
+            return c  # 返回最单一的类别
+        f = self._select_feature(X, Y, indices, features)  # 选择香农熵最小的特征
+        trees = {}
+        features_ = features[features != f]  # 除去选择的特征
+        for v in np.unique(X[indices, f]).tolist():  # 为该特征的每一个取值都建立子树
+            indices_ = self._query_indices(X, Y, indices, f, v)
+            trees[v] = self._create_tree(X, Y, indices_, features_)  # 递归构建子决策树
+        return {'feature': f, 'trees': trees}
 
-    def _query_indices(self, indices, feature, value):
-        return indices[self._X[indices, feature] == value]
-
-    def _calc_info_gain(self, indices, feature):  # 计算信息增益
-        ent = self._calc_entropy(indices)  # 经验熵
-        cond_ent = self._calc_cond_entropy(indices, feature)  # 经验条件熵
-        return ent - cond_ent  # 信息增益
-
-    def _calc_entropy(self, indices):  # 计算经验熵
-        prob = np.bincount(self._Y[indices]) / len(indices)  # 采用二进制计数法，x必须为正整数向量
+    @staticmethod
+    def _calc_entropy(X, Y, indices):  # 计算经验熵
+        prob = np.bincount(Y[indices]) / len(indices)  # 采用二进制计数法，x必须为正整数向量
         prob = prob[prob != 0]  # 除去0概率
         return np.sum(prob * -np.log(prob))  # 经验熵
 
-    def _calc_cond_entropy(self, indices, feature):  # 计算条件熵
-        cond_ent = 0  # 经验条件熵
-        for val in np.unique(self._X[indices, feature]):
-            sub_indices = self._query_indices(indices, feature, val)
-            cond_ent += len(sub_indices) / len(indices) * self._calc_entropy(sub_indices)
-        return cond_ent  # 条件熵
+    @staticmethod
+    def _query_indices(X, Y, indices, feature, value):
+        return indices[X[indices, feature] == value]
 
-    def _select_class(self, indices):
-        prob = np.bincount(self._Y[indices]) / len(indices)  # 计算类别频率
+    @staticmethod
+    def _select_class(X, Y, indices):
+        prob = np.bincount(Y[indices]) / len(indices)  # 计算类别频率
         c = np.argmax(prob)
         return c, prob[c]  # 返回出现次数最多的类别，以及其频率
 
-    def _select_feature(self, indices, features):
+    @classmethod
+    def _calc_cond_entropy(cls, X, Y, indices, feature):  # 计算条件熵
+        cond_ent = 0  # 经验条件熵
+        for v in np.unique(X[indices, feature]):
+            indices_ = cls._query_indices(X, Y, indices, feature, v)
+            cond_ent += len(indices_) / len(indices) * cls._calc_entropy(X, Y, indices_)
+        return cond_ent  # 条件熵
+
+    @classmethod
+    def _calc_info_gain(cls, X, Y, indices, feature):  # 计算信息增益
+        ent = cls._calc_entropy(X, Y, indices)  # 经验熵
+        cond_ent = cls._calc_cond_entropy(X, Y, indices, feature)  # 经验条件熵
+        return ent - cond_ent  # 信息增益
+
+    @classmethod
+    def _select_feature(cls, X, Y, indices, features):
         gains = np.array([
-            self._calc_info_gain(indices, feature) for feature in features
+            cls._calc_info_gain(X, Y, indices, f) for f in features
         ])  # 计算features中所有特征的信息增益
-        f = np.argmax(gains)
-        return features[f]  # 返回信息增益最大的特征
+        return features[np.argmax(gains)]  # 返回信息增益最大的特征
 
 
 def load_data():
     x = np.array([
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 1, 1],
-
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 1],
-        [1, 0, 1],
-        [1, 1, 0],
-        [1, 1, 0],
-        [1, 1, 1],
-        [1, 1, 1],
+        [0, 0, 0], [0, 0, 0], [0, 0, 1], [0, 0, 1], [0, 1, 0], [0, 1, 0], [0, 1, 1], [0, 1, 1],
+        [1, 0, 0], [1, 0, 0], [1, 0, 1], [1, 0, 1], [1, 1, 0], [1, 1, 0], [1, 1, 1], [1, 1, 1],
     ])
-    y = np.array([1 if np.sum(xi) >= 2 else 0 for xi in x])
+    y = np.where(x.sum(axis=1) >= 2, 1, 0)
     return x, y
 
 
 if __name__ == '__main__':
     x, y = load_data()
+
     decision_tree = DecisionTree(rate=0.95)
     decision_tree.fit(x, y)
+    pred = decision_tree(x)
+
     print(decision_tree.tree)
-    pred = decision_tree.predict(x)
     print(y)
     print(pred)
+
     acc = np.sum(pred == y) / len(pred)
     print(f'Acc = {100 * acc:.2f}%')
