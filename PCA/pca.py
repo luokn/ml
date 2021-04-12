e1d8a65719646fb8a782942bd20daa4a5da10eaf
Luokun <olooook@outlook.com>
2021-04-12 19:30:01 +0800

    PCA/pca: 完善

--- PCA/pca.py
+++ PCA/pca.py
@@ -18,7 +18,7 @@ class PCA:
         """
         self.k = k
 
-    def transform(self, X: np.ndarray):
+    def __call__(self, X: np.ndarray):
         X_norm = X - X.mean(axis=0)  # 去中心化
         L, U = np.linalg.eig(X_norm.T @ X_norm)  # 对协方差矩阵进行特征值分解
         topk = np.argsort(L)[::-1][:self.k]  # 找出特征值中前K大特征对应的索引
@@ -26,40 +26,48 @@ class PCA:
 
 
 def load_data():
-    x = np.random.randn(3, 200, 2)
-    x[1] += np.array([-2, 0])
-    x[2] += np.array([2, 0])
-
-    scale = np.diag([1.2, .6])  # 缩放矩阵
+    x = np.concatenate([np.random.randn(200, 2) + np.array([2, 0]),
+                        np.random.randn(200, 2),
+                        np.random.randn(200, 2) + np.array([-2, 0])])
     theta = np.pi / 4  # 逆时针旋转45°
-    rotate = np.array([
-        [np.cos(theta), -np.sin(theta)],
-        [np.sin(theta), np.cos(theta)]
-    ])  # 旋转矩阵
-
-    x = x.reshape(-1, 2)
-    x = x @ scale @ rotate.T
-    return x
-
-
-def plot_scatter(xys, title):
-    plt.figure(figsize=[8, 8])
-    for xy, color in zip(xys, ['r', 'g', 'b']):
-        plt.scatter(xy[:, 0], xy[:, 1], color=color, marker='.')
-    plt.xlim(-5, 5)
-    plt.ylim(-5, 5)
-    plt.title(title)
-    plt.show()
+    scale = np.diag([1.2, .6])  # 缩放矩阵
+    rotate = np.array([[np.cos(theta), -np.sin(theta)],
+                       [np.sin(theta), np.cos(theta)]])  # 旋转矩阵
+    return x @ scale @ rotate.T
 
 
 if __name__ == '__main__':
     x = load_data()
-    plot_scatter(x.reshape(3, -1, 2), 'Before PCA')
+    z = x.reshape(3, -1, 2)
+
+    plt.figure(figsize=[15, 5])
+    plt.subplot(1, 3, 1)
+    plt.title('Real')
+    plt.xlim(-5, 5)
+    plt.ylim(-5, 5)
+    plt.scatter(z[0, :, 0], z[0, :, 1], color='r', marker='.')
+    plt.scatter(z[1, :, 0], z[1, :, 1], color='g', marker='.')
+    plt.scatter(z[2, :, 0], z[2, :, 1], color='b', marker='.')
 
     # 不降维
-    x_2d = PCA(2).transform(x)
-    plot_scatter(x_2d.reshape(3, -1, 2), 'PCA 2D')
+    x_2d = PCA(2)(x)
+    z = x_2d.reshape(3, -1, 2)
+    plt.subplot(1, 3, 2)
+    plt.title('PCA 2D')
+    plt.xlim(-5, 5)
+    plt.ylim(-5, 5)
+    plt.scatter(z[0, :, 0], z[0, :, 1], color='r', marker='.')
+    plt.scatter(z[1, :, 0], z[1, :, 1], color='g', marker='.')
+    plt.scatter(z[2, :, 0], z[2, :, 1], color='b', marker='.')
 
     # 降为1维
-    x_1d = PCA(1).transform(x)
-    plot_scatter(np.concatenate([x_1d.reshape(3, -1, 1), np.zeros([3, 200, 1])], axis=-1), 'PCA 1D')
+    x_1d = PCA(1)(x)
+    z = x_1d.reshape(3, -1)
+    plt.subplot(1, 3, 3)
+    plt.title('PCA 1D')
+    plt.xlim(-5, 5)
+    plt.ylim(-5, 5)
+    plt.scatter(z[0], np.zeros([200]), color='r', marker='.')
+    plt.scatter(z[1], np.zeros([200]), color='g', marker='.')
+    plt.scatter(z[2], np.zeros([200]), color='b', marker='.')
+    plt.show()
