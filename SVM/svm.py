4b4aff2233f559815abc2ab24d20c56d8fbce1d3
Luokun <olooook@outlook.com>
2021-04-13 15:25:32 +0800

    SVM/svm: 改进SVM

--- SVM/svm.py
+++ SVM/svm.py
@@ -12,27 +12,27 @@ class SVM:
     Support Vector Machines(支持向量机)
     """
 
-    def __init__(self, C=1.0, tol=1e-3, max_iter=100, kernel='linear', **kwargs):
+    def __init__(self, C=1.0, tol=1e-3, iterations=100, kernel='linear', **kwargs):
         """
         :param C: 惩罚因子
         :param tol: 绝对误差限
         :param max_iter: 最大迭代次数
         :param kernel: 核函数
         """
-        self.C, self.tol, self.max_iter = C, tol, max_iter
+        self.C, self.tol, self.iterations = C, tol, iterations
         if kernel == 'linear':
-            self.K = self._LinearKernel()  # 线性核函数
+            self.K = LinearKernel()  # 线性核函数
         if kernel == 'poly':
-            self.K = self._PolyKernel()  # 多项式核函数
+            self.K = PolyKernel()  # 多项式核函数
         if kernel == 'rbf':
-            self.K = self._RBFKernel(kwargs['sigma'])  # 径向基核函数
+            self.K = RBFKernel(kwargs['sigma'])  # 径向基核函数
         self.alpha, self.b = None, .0
         self._X, self._Y = None, None
 
     def fit(self, X: np.ndarray, Y: np.ndarray):
         self._X, self._Y = X, Y
         self.alpha = np.ones([len(X)], dtype=np.float)  # 拉格朗日乘子
-        for _ in range(self.max_iter):
+        for _ in range(self.iterations):
             E = np.array([self._calc_error(i) for i in range(len(X))])  # 此次迭代缓存的误差
             for i1 in range(len(X)):  # 外层循环，寻找第一个alpha
                 E1 = self._calc_error(i1)  # 计算误差(不使用E缓存)
@@ -78,7 +78,7 @@ class SVM:
                 E[i1] = self._calc_error(i1)
                 E[i2] = self._calc_error(i2)
 
-    def predict(self, X: np.ndarray):
+    def __call__(self, X: np.ndarray):
         Y = np.array([self._g(x) for x in X])
         return np.where(Y > 0, 1, -1)  # 将(-\infinity, \infinity)之间的分布转为{-1, +1}标签
 
@@ -100,53 +100,65 @@ class SVM:
             return gi * yi <= 1
         return np.abs(gi * yi - 1) < self.tol
 
-    class _LinearKernel:
-        def __call__(self, x: np.ndarray, y: np.ndarray):
-            return np.sum(x * y, axis=-1)
 
-    class _PolyKernel:
-        def __call__(self, x: np.ndarray, y: np.ndarray):
-            return (np.sum(x * y, axis=-1) + 1) ** 2
+class LinearKernel:
+    def __call__(self, x: np.ndarray, y: np.ndarray):
+        return np.sum(x * y, axis=-1)
 
-    class _RBFKernel:
-        def __init__(self, sigma):
-            self.divisor = 2 * sigma ** 2
 
-        def __call__(self, x: np.ndarray, y: np.ndarray):
-            return np.exp(-np.sum((x - y) ** 2, axis=-1) / self.divisor)
+class PolyKernel:
+    def __call__(self, x: np.ndarray, y: np.ndarray):
+        return (np.sum(x * y, axis=-1) + 1) ** 2
+
+
+class RBFKernel:
+    def __init__(self, sigma):
+        self.divisor = 2 * sigma ** 2
+
+    def __call__(self, x: np.ndarray, y: np.ndarray):
+        return np.exp(-np.sum((x - y) ** 2, axis=-1) / self.divisor)
 
 
 def load_data():
-    x, y = np.random.randn(2, 400, 2), np.zeros([2, 400], dtype=int)
-    y[0] = -1
-    y[1] = 1
+    x0, x1 = np.random.randn(40, 10, 2), np.random.randn(400, 2),
+    y = np.stack([np.full([400], -1), np.full([400], 1)])
     for i, theta in enumerate(np.linspace(0, 2 * np.pi, 40)):
-        x[0, (10 * i):(10 * i + 10)] += 5 * np.array([np.cos(theta), np.sin(theta)])
+        x0[i] += 4 * np.array([np.cos(theta), np.sin(theta)])
+    x = np.stack([x0.reshape(-1, 2), x1])
     return x, y
-
-
-def plot_scatter(xys, title):
-    plt.figure(figsize=(8, 8))
-    for xy, color in zip(xys, ['r', 'g', 'b']):
-        plt.scatter(xy[:, 0], xy[:, 1], color=color)
-    plt.title(title)
-    plt.show()
 
 
 if __name__ == '__main__':
     x, y = load_data()
-    x = x.reshape(-1, 2)
-    y = y.flatten()
-    plot_scatter([x[y == i] for i in [-1, 1]], 'Real')
+    plt.figure(figsize=[18, 6])
+    plt.subplot(1, 3, 1)
+    plt.title('Real')
+    plt.xlim(-7, 7)
+    plt.ylim(-7, 7)
+    plt.scatter(x[0, :, 0], x[0, :, 1], color='r', marker='.')
+    plt.scatter(x[1, :, 0], x[1, :, 1], color='g', marker='.')
 
-    # train
-    svm = SVM(C=10, sigma=1, kernel='rbf', max_iter=100)
+    x, y = x.reshape(-1, 2), y.flatten()
+    svm = SVM(C=10, kernel='rbf', sigma=5, iterations=500)
     svm.fit(x, y)
-
-    # predict
-    pred = np.array(svm.predict(x))
-    print(svm.support_vectors)
-    plot_scatter([x[pred == i] for i in [-1, 1]], 'Pred')
-
+    pred = svm(x)
     acc = np.sum(pred == y) / len(pred)
     print(f'Acc = {100 * acc:.2f}%')
+
+    z = x[pred == -1], x[pred == 1]
+    plt.subplot(1, 3, 2)
+    plt.title('Pred')
+    plt.xlim(-7, 7)
+    plt.ylim(-7, 7)
+    plt.scatter(z[0][:, 0], z[0][:, 1], color='r', marker='.')
+    plt.scatter(z[1][:, 0], z[1][:, 1], color='g', marker='.')
+
+    v = svm.support_vectors
+    plt.subplot(1, 3, 3)
+    plt.title('Support vectors')
+    plt.xlim(-7, 7)
+    plt.ylim(-7, 7)
+    plt.scatter(z[0][:, 0], z[0][:, 1], color='r', marker='.')
+    plt.scatter(z[1][:, 0], z[1][:, 1], color='g', marker='.')
+    plt.scatter(v[:, 0], v[:, 1], color='b', marker='.')
+    plt.show()
