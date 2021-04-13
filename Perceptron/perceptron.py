5f07628ca7975aeb8d2f4d250179bc3222db45a8
Luokun <olooook@outlook.com>
2021-04-13 09:08:43 +0800

    Perceptron/perceptron: 优化算法

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Date  : 2020/5/20
+# @Author: Luokun
+# @Email : olooook@outlook.com
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -21,7 +26,7 @@ class Perceptron:
                 neg_grad = x * y  # 计算weights的负梯度
                 self.weights += self.lr * neg_grad  # 沿负梯度方向更新weights
 
-    def predict(self, X: np.ndarray):
+    def __call__(self, X: np.ndarray):
         pred = self._pad(X) @ self.weights
         return np.where(pred > 0, 1, -1)
 
@@ -31,62 +36,48 @@ class Perceptron:
 
 
 def load_data():
-    x, y = np.random.randn(2, 500, 2), np.zeros([2, 500], dtype=int)
-    x[0] += np.array([1, -1])
-    x[1] += np.array([-1, 1])
-    y[0] = -1
-    y[1] = 1
+    x = np.stack([np.random.randn(500, 2) + np.array([1, -1]),
+                  np.random.randn(500, 2) + np.array([-1, 1])])
+    y = np.stack([np.full([500], -1), np.full([500], 1)])
     return x, y
 
 
 def train_perceptron(model, x, y, epochs):
     indices = np.arange(len(x))
-    for epoch in range(epochs):
+    for _ in range(epochs):
         np.random.shuffle(indices)
-        shf_x, shf_y = x[indices], y[indices]
-        model.fit(shf_x, shf_y)
-
-
-def plot_scatter(xy0, xy1, title):
-    plt.figure(figsize=[8, 8])
-    plt.scatter(xy0[:, 0], xy0[:, 1], color='r', marker='.')
-    plt.scatter(xy1[:, 0], xy1[:, 1], color='g', marker='.')
-    plt.xlim(-5, 5)
-    plt.ylim(-5, 5)
-    plt.title(title)
-    plt.show()
-
-
-def plot_scatter_with_line(xy0, xy1, weights, title):
-    plt.figure(figsize=[8, 8])
-    plt.scatter(xy0[:, 0], xy0[:, 1], color='r', marker='.')
-    plt.scatter(xy1[:, 0], xy1[:, 1], color='g', marker='.')
-    plt.xlim(-5, 5)
-    plt.ylim(-5, 5)
-    plt.title(title)
-
-    # plot the dividing line
-    ln_x = np.linspace(-5, 5, 100)
-    ln_a = - weights[0] / weights[1]
-    ln_b = - weights[2] / weights[1]
-    ln_y = ln_a * ln_x + ln_b
-    plt.plot(ln_x, ln_y, color='b', linewidth=1)
-    plt.show()
+        model.fit(x[indices], y[indices])
 
 
 if __name__ == '__main__':
     x, y = load_data()
-    plot_scatter(x[0], x[1], 'Real')
 
-    # train
-    x = x.reshape(-1, 2)
-    y = y.flatten()
+    plt.figure(figsize=[12, 6])
+    plt.subplot(1, 2, 1)
+    plt.title('Real')
+    plt.xlim(-5, 5)
+    plt.ylim(-5, 5)
+    plt.scatter(x[0, :, 0], x[0, :, 1], color='r', marker='.')
+    plt.scatter(x[1, :, 0], x[1, :, 1], color='g', marker='.')
+
+    x, y = x.reshape(-1, 2), y.flatten()
     perceptron = Perceptron(input_dim=2, lr=1e-4)
-    train_perceptron(perceptron, x, y, epochs=100)
+    train_perceptron(perceptron, x, y, epochs=500)
 
-    # predict
-    pred = perceptron.predict(x)
-    plot_scatter_with_line(x[pred == -1], x[pred == 1], perceptron.weights, 'Pred')
-
+    pred = perceptron(x)
     acc = np.sum(pred == y) / len(pred)
     print(f'Acc = {100 * acc:.2f}%')
+    z = x[pred == -1], x[pred == 1]
+    plt.subplot(1, 2, 2)
+    plt.title('Pred')
+    plt.xlim(-5, 5)
+    plt.ylim(-5, 5)
+    plt.scatter(z[0][:, 0], z[0][:, 1], color='r', marker='.')
+    plt.scatter(z[1][:, 0], z[1][:, 1], color='g', marker='.')
+
+    w = perceptron.weights
+    a, b = - w[0] / w[1], - w[2] / w[1]
+    line_x = np.linspace(-5, 5, 100)
+    line_y = a * line_x + b
+    plt.plot(line_x, line_y, color='b', linewidth=1)
+    plt.show()
