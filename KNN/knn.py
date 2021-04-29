f1c376324d7ae16f4d2c3cdac4a51f89fac5510f
Luokun <olooook@outlook.com>
2021-04-29 17:46:37 +0800

    .

--- KNN/knn.py
+++ KNN/knn.py
@@ -17,17 +17,17 @@ class KNN:
         :param K: 分类近邻数
         """
         self.k = k
-        self.X, self.Y = None, None
+        self._X, self._Y = None, None
 
     def fit(self, X: np.ndarray, Y: np.ndarray):
-        self.X, self.Y = X, Y  # 训练集X与Y，类别已知
+        self._X, self._Y = X, Y  # 训练集X与Y，类别已知
 
     def __call__(self, X: np.ndarray):
         Y = np.zeros([len(X)], dtype=int)  # X对应的类别输出变量
         for i, x in enumerate(X):
-            dist = np.linalg.norm(self.X - x, axis=1)  # 计算x与所有已知类别点的距离
+            dist = np.linalg.norm(self._X - x, axis=1)  # 计算x与所有已知类别点的距离
             topk = np.argsort(dist)[:self.k]  # 取距离最近的k个点对应的索引
-            counter = np.bincount(self.Y[topk])  # 统计k近邻点的类别数量
+            counter = np.bincount(self._Y[topk])  # 统计k近邻点的类别数量
             Y[i] = np.argmax(counter)  # k近邻次数最多的类别将作为x的类别
         return Y
 
