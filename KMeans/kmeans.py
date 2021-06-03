22494fa52e25c4886058a7228252d6f7211665de
Luokun <olooook@outlook.com>
2021-06-03 10:17:58 +0800

    .

--- KMeans/kmeans.py
+++ KMeans/kmeans.py
@@ -14,21 +14,21 @@ class KMeans:
     K-means clustering(K均值聚类)
     """
 
-    def __init__(self, K: int, eps=1e-3, iterations=100):
+    def __init__(self, k: int, eps=1e-3, iterations=100):
         """
         :param K: 聚类类别数
         :param eps: 中心点最小更新量
-        :param max_iter: 迭代最大次数
+        :param iterations: 迭代最大次数
         """
-        self.K, self.eps, self.iterations = K, eps, iterations
+        self.k, self.eps, self.iterations = k, eps, iterations
         self.centers = None  # 中心点
 
     def fit(self, X: np.ndarray):
-        self.centers = X[random.sample(range(len(X)), self.K)]  # 随机选择k个点作为中心点
+        self.centers = X[random.sample(range(len(X)), self.k)]  # 随机选择k个点作为中心点
         for _ in range(self.iterations):  # 达到最大迭代次数iterations退出迭代
             Y = self.__call__(X)  # 更新节点类别
             means = np.empty_like(self.centers)  # 各类别点的均值
-            for k in range(self.K):
+            for k in range(self.k):
                 if np.any(Y == k):  # 存在元素属于类别i
                     means[k] = np.mean(X[Y == k], axis=0)  # 计算类别i所有点的均值
                 else:  # 不存在任何元素属于类别i
