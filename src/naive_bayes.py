3c8a9658f5283ec4e1f3f7352b627d70001750f5
Kun Luo <luokun485@gmail.com>
2022-05-21 15:10:46 +0800

    修改了命名

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -12,44 +12,43 @@ class NaiveBayesClassifier:
     """
 
     def __init__(self):
-        self.prior_prob, self.cond_prob = None, None
+        self.P_prior, self.P_cond = None, None
 
-    def fit(self, X: np.ndarray, Y: np.ndarray):
+    def fit(self, X: np.ndarray, y: np.ndarray):
         # 计算X的各个特征与标签Y有多少类别
-        x_categories, y_categories = np.max(X, axis=0) + 1, np.max(Y) + 1
+        x_categories, y_categories = np.max(X, axis=0) + 1, np.max(y) + 1
         # 先验概率, prior_prob[i] = P(Y = i)，标签类别的取值概率
-        self.prior_prob = self._estimate_prob(Y, y_categories)
+        self.P_prior = self.estimate_prob(y, y_categories)
         # 条件概率, cond_prob[k][i,j] = P(X_i = a_{ij} | Y = k)，标签类别为k的条件下i特征取a_{ij}的概率
-        self.cond_prob = [np.zeros([X.shape[1], n]) for n in x_categories]
+        self.P_cond = [np.zeros([X.shape[1], n]) for n in x_categories]
         for k in range(y_categories):
             for i, n in enumerate(x_categories):  # 第i个特征有n_i个类别
-                self.cond_prob[k][i] = self._estimate_prob(X[Y == k, i], n)  # 类别为k时，i特征所有取值的概率
+                self.P_cond[k][i] = self.estimate_prob(X[y == k, i], n)  # 类别为k时，i特征所有取值的概率
 
-    def __call__(self, X: np.ndarray):
-        Y = np.zeros([len(X)], dtype=int)
+    def __call__(self, X: np.ndarray) -> np.ndarray:
+        y_pred = np.zeros([len(X)], dtype=int)
         for i, x in enumerate(X):
-            prob = np.log(self.prior_prob) + np.array(
-                [np.log(cond_prob[range(len(x)), x]).sum() for cond_prob in self.cond_prob]
+            P = np.log(self.P_prior) + np.array(
+                [np.log(p_cond[np.arange(len(x)), x]).sum() for p_cond in self.P_cond]
             )  # 先验概率的对数,加上条件概率的对数
-            Y[i] = np.argmax(prob)
-        return Y
+            y_pred[i] = np.argmax(P)
+        return y_pred
 
     def predict_prob(self, X: np.ndarray):
-        prob = np.zeros([len(X), len(self.cond_prob)])
+        P = np.zeros([len(X), len(self.P_cond)])
         for i, x in enumerate(X):
-            for c, prior_prob, cond_prob in zip(range(len(self.cond_prob)), self.prior_prob, self.cond_prob):
-                print(f"prior_prob = {prior_prob}, cond_prob = {cond_prob[range(len(x)), x]}")
-                prob[i, c] = prior_prob * np.prod(cond_prob[range(len(x)), x])
-        return prob
+            for cat, p_prior, p_cond in zip(range(len(self.P_cond)), self.P_prior, self.P_cond):
+                P[i, cat] = p_prior * np.prod(p_cond[range(len(x)), x])
+        return P
 
     @staticmethod
-    def _estimate_prob(x: np.ndarray, n: int):
+    def estimate_prob(x: np.ndarray, n: int):
         return (np.bincount(x, minlength=n) + 1) / (len(x) + n)  # 使用贝叶斯估计
 
 
 def load_data():
     # 参照李航《统计学习方法（第一版）》第四章例4.1
-    x = np.array(
+    X = np.array(
         [
             [0, 0],
             [0, 1],
@@ -70,25 +69,24 @@ def load_data():
         dtype=int,
     )
     y = np.array([0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0], dtype=int)
-    return x, y
+    return X, y
 
 
 if __name__ == "__main__":
-    x, y = load_data()
+    X, y = load_data()
     naive_bayes = NaiveBayesClassifier()
-    naive_bayes.fit(x, y)
-    pred = naive_bayes(x)
+    naive_bayes.fit(X, y)
+    pred = naive_bayes(X)
 
-    print(naive_bayes.prior_prob)  # 先验概率
+    print(naive_bayes.P_prior)  # 先验概率
     # [7/17, 10/17]
-    print(naive_bayes.cond_prob[0])  # 条件概率
+    print(naive_bayes.P_cond[0])  # 条件概率
     # [[4/9, 3/9, 2/9]
     #  [4/9, 3/9, 2/9]]
-    print(naive_bayes.cond_prob[1])  # 条件概率
+    print(naive_bayes.P_cond[1])  # 条件概率
     # [[3/12, 4/12, 5/12]
     #  [2/12, 5/12, 5/12]]
     acc = np.sum(pred == y) / len(pred)
     print(f"Accuracy = {100 * acc:.2f}%")
-    print()
     print(naive_bayes.predict_prob([[1, 0]]))  # 输出 [[1, 0]]的概率
     # [[0.061, 0.0327]]
