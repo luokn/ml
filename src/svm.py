f9f85825b95e84746d9029f40b2bb06135e7ac89
Kun Luo <luokun485@gmail.com>
2022-05-21 14:06:10 +0800

    。

--- src/svm.py
+++ src/svm.py
@@ -7,47 +7,78 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+class LinearKernel:  # 线性核函数
+    def __call__(self, a: np.ndarray, b: np.ndarray):
+        return np.sum(a * b, axis=-1)
+
+
+class PolyKernel:  # 多项式核函数
+    def __call__(self, a: np.ndarray, b: np.ndarray):
+        return (np.sum(a * b, axis=-1) + 1) ** 2
+
+
+class RBFKernel:  # 高斯核函数
+    def __init__(self, sigma):
+        self.divisor = 2 * sigma**2
+
+    def __call__(self, a: np.ndarray, b: np.ndarray):
+        return np.exp(-np.sum((a - b) ** 2, axis=-1) / self.divisor)
+
+
 class SVM:
     """
     Support Vector Machines(支持向量机)
     """
 
-    def __init__(self, C=1.0, kernel="linear", iterations=100, tol=1e-3, **kwargs):
+    def __init__(self, kernel="linear", C=1.0, iterations=100, tol=1e-3, sigma=1.0):
         """
         Args:
-            C (float, optional): 惩罚因子. Defaults to 1.0.
             kernel (str, optional): 核函数. Defaults to 'linear'.
+            C (float, optional): 惩罚因子. Defaults to 1.0.
             iterations (int, optional): 最大迭代次数. Defaults to 100.
             tol (float, optional): 绝对误差限. Defaults to 1e-3.
+            sigma (float, optional): 高斯核函数的sigma. Defaults to 1.0.
         """
         assert kernel in ["linear", "poly", "rbf"]
-        self.C, self.iterations, self.tol = C, iterations, tol
+
         if kernel == "linear":
             self.K = LinearKernel()  # 线性核函数
         if kernel == "poly":
             self.K = PolyKernel()  # 多项式核函数
         if kernel == "rbf":
-            self.K = RBFKernel(kwargs["sigma"])  # 径向基核函数
-        self.alpha, self.b = None, 0.0
-        self.X, self.Y = None, None
+            self.K = RBFKernel(sigma)  # 径向基核函数
 
-    def fit(self, X: np.ndarray, Y: np.ndarray):
-        self.X, self.Y = X, Y
-        self.alpha = np.ones([len(X)])  # 拉格朗日乘子
+        self.C, self.iterations, self.tol, self.alpha, self.b = C, iterations, tol, None, 0.0
+
+        self.X, self.y = None, None
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        self.X, self.y = X, y
+
+        # 拉格朗日乘子
+        self.alpha = np.ones([len(X)])
         for _ in range(self.iterations):
-            E = np.array([self._calc_error(i) for i in range(len(X))])  # 此次迭代缓存的误差
-            for i1 in range(len(X)):  # 外层循环，寻找第一个alpha
-                E1 = self._calc_error(i1)  # 计算误差(不使用E缓存)
-                if E1 == 0 or self._satisfy_kkt(i1):  # 误差为0或满足KKT条件
+            # 此次迭代缓存的误差
+            E = np.array([self.__E(i) for i in range(len(X))])
+
+            # 外层循环，寻找第一个alpha
+            for i1 in range(len(X)):
+
+                # 计算误差(不使用E缓存)
+                E1 = self.__E(i1)
+                if E1 == 0 or self._satisfy_kkt(i1):
+                    # 误差为0或满足KKT条件
                     continue
+
                 # 大于0则选择最小,小于0选择最大的
                 i2 = np.argmin(E) if E1 > 0 else np.argmax(E)  # 内层循环，寻找第二个alpha
                 if i1 == i2:
                     continue
-                E2 = self._calc_error(i2)
-                x1, x2, y1, y2 = X[i1], X[i2], Y[i1], Y[i2]
+                E2 = self.__E(i2)
+                x1, x2, y1, y2 = X[i1], X[i2], y[i1], y[i2]
                 alpha1, alpha2 = self.alpha[i1], self.alpha[i2]
                 k11, k22, k12 = self.K(x1, x1), self.K(x2, x2), self.K(x1, x2)
+
                 # 计算剪切范围
                 if y1 * y2 < 0:
                     L = max(0, alpha2 - alpha1)
@@ -60,13 +91,16 @@ class SVM:
                 eta = k11 + k22 - 2 * k12
                 if eta <= 0:
                     continue
+
                 # 计算新alpha
                 alpha2_new = np.clip(alpha2 + y2 * (E1 - E2) / eta, L, H)
                 alpha1_new = alpha1 + y1 * y2 * (alpha2 - alpha2_new)
+
                 # 计算新b
                 alpha2_delta, alpha1_delta = alpha2_new - alpha2, alpha1_new - alpha1
                 b1_new = -E1 - y1 * k11 * alpha1_delta - y2 * k12 * alpha2_delta + self.b
                 b2_new = -E2 - y1 * k12 * alpha1_delta - y2 * k22 * alpha2_delta + self.b
+
                 # 更新参数
                 self.alpha[i1] = alpha1_new
                 self.alpha[i2] = alpha2_new
@@ -76,94 +110,81 @@ class SVM:
                     self.b = b2_new
                 else:
                     self.b = (b1_new + b2_new) / 2
+
                 # 更新误差缓存
-                E[i1] = self._calc_error(i1)
-                E[i2] = self._calc_error(i2)
+                E[i1] = self.__E(i1)
+                E[i2] = self.__E(i2)
 
     def __call__(self, X: np.ndarray):
-        Y = np.array([self._g(x) for x in X])
-        return np.where(Y > 0, 1, -1)  # 将(-\infinity, \infinity)之间的分布转为{-1, +1}标签
+        pred = np.array([self.__g(x) for x in X])
+        return np.where(pred > 0, 1, -1)  # 将(-\infinity, \infinity)之间的分布转为{-1, +1}标签
 
     @property
     def support_vectors(self):  # 支持向量
         return self.X[self.alpha > 0]
 
-    def _g(self, x):  # g(x) =\sum_{i=0}^N alpha_i y_i \kappa(x_i, x)
-        return np.sum(self.alpha * self.Y * self.K(self.X, x)) + self.b
+    def __g(self, x):  # g(x) =\sum_{i=0}^N alpha_i y_i \kappa(x_i, x)
+        return np.sum(self.alpha * self.y * self.K(self.X, x)) + self.b
 
-    def _calc_error(self, i):  # E_i = g(x_i) - y_i
-        return self._g(self.X[i]) - self.Y[i]
+    def __E(self, i):  # E_i = g(x_i) - y_i
+        return self.__g(self.X[i]) - self.y[i]
 
     def _satisfy_kkt(self, i):  # 是否满足KKT条件
-        gi, yi = self._g(self.X[i]), self.Y[i]
+        g_i, y_i = self.__g(self.X[i]), self.y[i]
         if np.abs(self.alpha[i]) < self.tol:
-            return gi * yi >= 1
+            return g_i * y_i >= 1
         if np.abs(self.alpha[i]) > self.C - self.tol:
-            return gi * yi <= 1
-        return np.abs(gi * yi - 1) < self.tol
-
-
-class LinearKernel:
-    def __call__(self, x: np.ndarray, y: np.ndarray):
-        return np.sum(x * y, axis=-1)
-
-
-class PolyKernel:
-    def __call__(self, x: np.ndarray, y: np.ndarray):
-        return (np.sum(x * y, axis=-1) + 1) ** 2
-
-
-class RBFKernel:
-    def __init__(self, sigma):
-        self.divisor = 2 * sigma ** 2
-
-    def __call__(self, x: np.ndarray, y: np.ndarray):
-        return np.exp(-np.sum((x - y) ** 2, axis=-1) / self.divisor)
+            return g_i * y_i <= 1
+        return np.abs(g_i * y_i - 1) < self.tol
 
 
 def load_data():
-    x0, x1 = (
-        np.random.randn(40, 10, 2),
-        np.random.randn(400, 2),
-    )
-    y = np.stack([np.full([400], -1), np.full([400], 1)])
+    X0, X1 = np.random.randn(40, 10, 2), np.random.randn(400, 2)
+
     for i, theta in enumerate(np.linspace(0, 2 * np.pi, 40)):
-        x0[i] += 4 * np.array([np.cos(theta), np.sin(theta)])
-    x = np.stack([x0.reshape(-1, 2), x1])
-    return x, y
+        X0[i] += 4 * np.array([np.cos(theta), np.sin(theta)])
+
+    X = np.stack([X0.reshape(-1, 2), X1])
+    y = np.stack([np.full([400], -1), np.full([400], 1)])
+    return X, y
 
 
 if __name__ == "__main__":
-    x, y = load_data()
-    plt.figure(figsize=[18, 6])
+    X, y = load_data()
+
+    plt.figure(figsize=[15, 5])
     plt.subplot(1, 3, 1)
     plt.title("Ground Truth")
     plt.xlim(-7, 7)
     plt.ylim(-7, 7)
-    plt.scatter(x[0, :, 0], x[0, :, 1], color="r", marker=".")
-    plt.scatter(x[1, :, 0], x[1, :, 1], color="g", marker=".")
+    plt.scatter(X[0, :, 0], X[0, :, 1], color="r", marker=".")
+    plt.scatter(X[1, :, 0], X[1, :, 1], color="g", marker=".")
 
-    x, y = x.reshape(-1, 2), y.flatten()
-    svm = SVM(C=10, kernel="rbf", sigma=5, iterations=500)
-    svm.fit(x, y)
-    pred = svm(x)
+    X, y = X.reshape(-1, 2), y.reshape(-1)
+
+    svm = SVM(kernel="rbf", C=10, iterations=500, sigma=5)
+    svm.fit(X, y)
+
+    pred = svm(X)
+
     acc = np.sum(pred == y) / len(pred)
     print(f"Accuracy = {100 * acc:.2f}%")
 
-    x0, x1 = x[pred == -1], x[pred == 1]
+    X_neg, X_pos = X[pred == -1], X[pred == 1]
     plt.subplot(1, 3, 2)
     plt.title("Prediction")
     plt.xlim(-7, 7)
     plt.ylim(-7, 7)
-    plt.scatter(x0[:, 0], x0[:, 1], color="r", marker=".")
-    plt.scatter(x1[:, 0], x1[:, 1], color="g", marker=".")
+    plt.scatter(X_neg[:, 0], X_neg[:, 1], color="r", marker=".")
+    plt.scatter(X_pos[:, 0], X_pos[:, 1], color="g", marker=".")
 
-    sv = svm.support_vectors
+    vectors = svm.support_vectors
     plt.subplot(1, 3, 3)
     plt.title("Support vectors")
     plt.xlim(-7, 7)
     plt.ylim(-7, 7)
-    plt.scatter(x0[:, 0], x0[:, 1], color="r", marker=".")
-    plt.scatter(x1[:, 0], x1[:, 1], color="g", marker=".")
-    plt.scatter(sv[:, 0], sv[:, 1], color="b", marker=".")
+    plt.scatter(X_neg[:, 0], X_neg[:, 1], color="r", marker=".")
+    plt.scatter(X_pos[:, 0], X_pos[:, 1], color="g", marker=".")
+    plt.scatter(vectors[:, 0], vectors[:, 1], color="b", marker=".")
+
     plt.show()
